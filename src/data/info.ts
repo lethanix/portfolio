d56bc47887db5f12a136dc68f4523de0af6a74e3
lethanix <louismurguia@proton.me>
2023-12-13 09:02:58 -0600

    Updated Description and Project sections

--- src/data/info.ts
+++ src/data/info.ts
@@ -2,15 +2,9 @@ export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
   name: "Louis Murguia",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
-  `,
+  about: `I'm a backend developer and cloud enthusiast.
+          I love learning new things and never stop exploring
+          new technologies.`,
 
   experience: [
     {
